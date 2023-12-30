5e7496319a54780d46b4460f32501d6474dfc241
ZXY-jp <pei8925@gmail.com>
2023-12-30 23:15:08 +0800

    3 type of  startrail
    三種星軌完成but當星星太大有光暈的時候會爛掉，且當圖片過大(200*2000以上)需要至少1分鐘

--- Combine.cpp
+++ Combine.cpp
@@ -1,41 +1,41 @@
 #include <opencv2/opencv.hpp>
 #include <string>
 #include <Vector>
+#include <time.h>
 #define PI 3.14159
-
+#define flag 3
 using namespace cv;
 using namespace std;
 
 void ImageCombine(const Mat& img, const Mat& trail_mask, const Mat& trail) {
-	//Mat keep;
 	cout << trail_mask.type() << endl;
 	cout << img.type() << endl;
-
-	//cvtColor(trail_mask, trail_mask, COLOR_GRAY2BGR);
-	//img.convertTo(img, CV_64F);
-
-
-	bitwise_and(img, trail_mask, trail_mask);
-
+	cout << trail.type() << endl;
+	bitwise_and(img, trail_mask, trail_mask);	//建立遮罩讓星軌更乾淨
 	//imshow("ROI", trail_mask);
 	//waitKey();
 
-	Mat result = Mat::zeros(img.size(), CV_64F);
+
+	Mat result = Mat::zeros(img.size(), CV_8UC3);
 	add(trail, trail_mask, result);
 	imshow("result", result);
 	waitKey();
 }
 
-Mat circleTrail(const Mat& img, Mat& result, double minutes) {
-	Point center(img.cols / 2, img.rows / 2);
+Mat StarTrail(const Mat& img, Mat& result, double minutes) {
+	Point center(img.cols / 4, img.rows / 2);
+	Mat rot_mat;
 	// 找最大星星
+	if (flag == 1) {
+		rot_mat = getRotationMatrix2D(center, 0.5, 1.0);
+	}
+	else if (flag == 2) {
+		rot_mat = getRotationMatrix2D(center, 0.8, 0.995);
+	}
+	else if (flag == 3) {
+		rot_mat = getRotationMatrix2D(center, 0, 0.998);
+	}
 
-
-
-
-
-
-	Mat rot_mat = getRotationMatrix2D(center, 0.5, 1.0);
 	Size img_size(img.cols, img.rows);
 	//int times = minutes / ;
 	Mat temp;
@@ -44,99 +44,51 @@ Mat circleTrail(const Mat& img, Mat& result, double minutes) {
 	// 使用BORDER_CONSTANT，並指定邊界顏色為黑色
 	for (int i = 0; i < 2 * minutes; i++) {
 		warpAffine(temp, temp, rot_mat, img_size, INTER_LINEAR);
-		//threshold(temp, temp, 200, 255, THRESH_BINARY);
 		bitwise_or(temp, result, result);
 	}
 
 	GaussianBlur(result, result, Size(3, 3), 0.2, 0.2);
-	//normalize(temp, temp, 50, 255);
-	imshow("Trail", result);
-	waitKey();
+	cvtColor(result, result, COLOR_GRAY2BGR);
+	cout << "Star Trail's type is: " << result.type() << endl;
 
-	Mat mask = Mat::zeros(result.size(), CV_8U);
-	threshold(result, mask, 50, 255,THRESH_BINARY_INV);
-	imshow("Mask", mask);
-	waitKey(0);	
+	Mat mask;
+	threshold(result, mask, 220, 255,THRESH_BINARY_INV);
+	cout << "mask's type is: " << mask.type() << endl;
+
+	imshow("Star Trail", result);
+	//imshow("Mask", mask);
+	//waitKey(0);	
 	return mask;
 }
 
-void psiralTrail(Mat& img, Mat& result, int angle) {
-	cout << "(寬, 高)" << img.cols <<  "  " << img.rows << endl;
-	Point center(img.cols/2, img.rows / 2);
-	//Point center(300, 480);
-
-	double radious, theta, dis, new_x, new_y;
-	double p, q, x0, y0, x1, y1;
-
-	for (int i = 0; i < img.rows; i++) {
-		for (int j = 0; j < img.cols; j++) {
-			y0 = center.y - i;
-			x0 = j - center.x;
-			dis = x0 * x0 + y0 * y0;		
-
-			theta = atan(y0 / (x0 + 0.0001));
-			if (x0 < 0)
-				theta += PI;
-			radious = sqrt(dis);
-			theta += radious / 70;
-			new_x = radious * cos(theta);
-			new_y = radious * sin(theta);
-			new_x = center.x + new_x;
-			new_y = center.y - new_y;
-			if (new_x < 0) new_x = 0;
-			if (new_x >= img.cols-1) new_x = img.cols - 2;
-			if (new_y < 0) new_y = 0;
-			if (new_y >= img.rows-1) new_y = img.rows - 2;
-
-			x1 = int(new_x);
-			y1 = int(new_y);
-
-			p = new_x - x1;
-			q = new_y - y1;
-
-			result.at<uchar>(i, j) =
-				(1 - p) * (1 - q) * img.at<uchar>(y1, x1) +
-				(p) * (1 - q) * img.at<uchar>(y1, x1 + 1) +
-				(1 - p) * (q)*img.at<uchar>(y1 + 1, x1) +
-				(p) * (q)*img.at<uchar>(y1 + 1, x1 + 1);
-		}
-	}
-
-	GaussianBlur(result, result, Size(5,5), 0.1, 0.1);
-	//normalize(temp, temp, 180, 255);
-	result.convertTo(result, CV_8U);
-	imshow("img", result);
-	waitKey();
-}
-
 Mat get_star(const cv::Mat img, cv::Mat& star_mask) {
-	cv::Scalar lowerBound(174, 159, 0);
-	cv::Scalar upperBound(255, 255, 255);
+	Scalar lowerBound(174, 159, 0);
+	Scalar upperBound(255, 255, 255);
 
 	// Create a binary mask (star_mask) based on the color range
-	cv::inRange(img, lowerBound, upperBound, star_mask);
+	inRange(img, lowerBound, upperBound, star_mask);
 
 	// Apply the binary mask to the original image
-	cv::Mat star_result;
-	cv::bitwise_and(img, img, star_result, star_mask);
-	cv::cvtColor(star_result, star_result, COLOR_BGR2GRAY);
+	Mat star_result;
+	bitwise_and(img, img, star_result, star_mask);
+	cvtColor(star_result, star_result, COLOR_BGR2GRAY);
 
 	// cv::imshow("Star Mask", star_result);
 
 	// Find contours in the image and cancel too big contour set as 200
-	std::vector<std::vector<cv::Point>> contours;
-	std::vector<cv::Vec4i> hierarchy;
+	vector<std::vector<cv::Point>> contours;
+	vector<cv::Vec4i> hierarchy;
 	Mat star_process = star_result.clone();
-	cv::dilate(star_process, star_process, cv::Mat());
-	cv::dilate(star_process, star_process, cv::Mat());
-	cv::dilate(star_process, star_process, cv::Mat());
-	cv::dilate(star_process, star_process, cv::Mat());
+	dilate(star_process, star_process, cv::Mat());
+	dilate(star_process, star_process, cv::Mat());
+	dilate(star_process, star_process, cv::Mat());
+	dilate(star_process, star_process, cv::Mat());
 
-	cv::findContours(star_process, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+	findContours(star_process, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
 	// Draw contours on the original image
-	cv::Mat contourImage = star_result.clone();
-	cv::Mat mask = cv::Mat::ones(star_result.size(), CV_8UC1) * 255;
+	Mat contourImage = star_result.clone();
+	Mat mask = cv::Mat::ones(star_result.size(), CV_8UC1) * 255;
 	//cv::drawContours(contourImage, contours, -1, cv::Scalar(0, 255, 0), 2);
 	//cout << contours.size() << endl;
 	for (int i = 0; i < contours.size(); ++i) {
@@ -245,12 +197,15 @@ cv::Mat customFloodFill(cv::Mat image, int x, int y, cv::Scalar fillColor) {
 
 int main()
 {
-	string fileName = "./img./aurora_5.jpg";
+	double START = clock();
+	string fileName = "./img./aurora_3.jpg";
 	Mat img = imread(fileName);
 
-	resize(img, img, Size(500, 500));
+	//resize(img, img, Size(img.cols, img.rows));
+	resize(img, img, Size(700,700));
+
 	Mat org_img = img;
-	cv::imshow("open", img);
+	//cv::imshow("open", img);
 
 	int blocksize = 10;
 	int gridWidth = img.cols / blocksize;
@@ -259,8 +214,8 @@ int main()
 
 	cv::Mat star_mask;
 	cv::Mat star_result = get_star(img, star_mask);
-	cv::imshow("starresult", star_result);
-	waitKey();
+	//cv::imshow("starresult", star_result);
+	//waitKey();
 
 
 	cv::cvtColor(img, img, COLOR_BGR2GRAY);
@@ -279,19 +234,19 @@ int main()
 	/*--------------------------------------------------------------*/
 
 	star_result.convertTo(star_result, CV_8U);
-	//cout << img.type() << endl;
 	Mat trail;
 	star_result.copyTo(trail);
 	trail.create(Size(img.cols, img.rows), CV_8U);
+	
 	Mat trail_mask;
-	trail_mask = circleTrail(star_result, trail, 60);
-	ImageCombine(img, trail_mask, trail);
-
+	int miniute = 120;
+	trail_mask = StarTrail(star_result, trail, miniute);
+	ImageCombine(org_img, trail_mask, trail);
+	double END = clock();
+	cout << "\n\nThe total time is: " << (END - START)/ CLOCKS_PER_SEC << " sec." << endl;
 
 	//psiralTrail(img, trail, 10);
 	//imwrite("startrail.png", trail);
-
-
 
 	/*--------------------------------------------------------------------*/
 	return 0;
